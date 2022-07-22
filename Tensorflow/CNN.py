2e1188e0bbab58e42fba6aba29126072700fe1a4
iman370 <imaniqbal@live.co.uk>
2022-07-22 22:06:24 +0100

    Getting started on convoluted neural networks

---
+++ Tensorflow/CNN.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+#  LOAD AND SPLIT DATASET
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+# Let's look at a one image
+IMG_INDEX = 79 # change this to look at other images
+
+plt.imshow(train_images[IMG_INDEX] ,cmap=plt.cm.binary)
+plt.xlabel(class_names[train_labels[IMG_INDEX][0]])
+plt.show()
