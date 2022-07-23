c35706baf8c9e083b8d94876057ee23849691149
iman370 <imaniqbal@live.co.uk>
2022-07-23 16:50:28 +0100

    Added data augmentation

--- Tensorflow/CNN.py
+++ Tensorflow/CNN.py
@@ -47,3 +47,33 @@ history = model.fit(train_images, train_labels, epochs=4,
 # Evaluating the model
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 print(test_acc)
+
+# Data Augmentation
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+# creates a data generator object that transforms images
+datagen = ImageDataGenerator(
+rotation_range=40,
+width_shift_range=0.2,
+height_shift_range=0.2,
+shear_range=0.2,
+zoom_range=0.2,
+horizontal_flip=True,
+fill_mode='nearest')
+
+# pick an image to transform
+test_img = train_images[20]
+img = image.img_to_array(test_img)  # convert image to numpy arry
+img = img.reshape((1,) + img.shape)  # reshape image
+
+i = 0
+
+for batch in datagen.flow(img, save_prefix='test', save_format='jpeg'):  # this loops runs forever until we break, saving images to current directory with specified prefix
+    plt.figure(i)
+    plot = plt.imshow(image.img_to_array(batch[0]))
+    i += 1
+    if i > 4:  # show 4 images
+        break
+
+plt.show()
