2c03a0f59d954a0fcd02c7664d82235f47f40559
iman370 <imaniqbal@live.co.uk>
2022-07-23 18:37:01 +0100

    Created the pretrained CNN

--- Tensorflow/Pretrained-CNN.py
+++ Tensorflow/Pretrained-CNN.py
@@ -64,3 +64,58 @@ for img, label in raw_train.take(2):
 for img, label in train.take(2):
   print("New shape:", img.shape)
 
+# Picking a pre-trained model
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+
+# Create the base model from the pre-trained model MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+base_model.summary()
+
+for image, _ in train_batches.take(1):
+   pass
+
+feature_batch = base_model(image)
+print(feature_batch.shape)
+
+# Freezing the base
+base_model.trainable = False
+
+# Adding our classifier
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+
+# Adding the prediction layer
+prediction_layer = keras.layers.Dense(1)
+
+# Combining it all
+model = tf.keras.Sequential([
+  base_model,
+  global_average_layer,
+  prediction_layer
+])
+
+# Training the model
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# We can evaluate the model right now to see how it does before training it on our new images
+initial_epochs = 3
+validation_steps=20
+
+loss0,accuracy0 = model.evaluate(validation_batches, steps = validation_steps)
+
+# Now we can train it on our images
+history = model.fit(train_batches,
+                    epochs=initial_epochs,
+                    validation_data=validation_batches)
+
+acc = history.history['accuracy']
+print(acc)
+
+# Saving the model
+model.save("dogs_vs_cats.h5")  # we can save the model and reload it at anytime in the future
+new_model = tf.keras.models.load_model('dogs_vs_cats.h5')
