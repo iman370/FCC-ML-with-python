6de440f99fae2e6ad5d3f43c18d944d3992f5502
iman370 <imaniqbal@live.co.uk>
2022-07-23 16:16:50 +0100

    Trained a CNN

--- Tensorflow/CNN.py
+++ Tensorflow/CNN.py
@@ -13,8 +13,37 @@ class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
 # Let's look at a one image
-IMG_INDEX = 79 # change this to look at other images
+#IMG_INDEX = 79 # change this to look at other images
+#
+#plt.imshow(train_images[IMG_INDEX] ,cmap=plt.cm.binary)
+#plt.xlabel(class_names[train_labels[IMG_INDEX][0]])
+#plt.show()
 
-plt.imshow(train_images[IMG_INDEX] ,cmap=plt.cm.binary)
-plt.xlabel(class_names[train_labels[IMG_INDEX][0]])
-plt.show()
+# Creating the architecture
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+#model.summary()  # let's have a look at our model so far
+
+# Adding dense layers
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10)) # 10 because we have 10 classes
+
+#model.summary()
+
+# Training the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=4, 
+                    validation_data=(test_images, test_labels))
+
+# Evaluating the model
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print(test_acc)
