d1587ec2d8406f5c886e93ffa06011d7d4889e8a
iman370 <imaniqbal@live.co.uk>
2022-07-26 12:55:59 +0100

    Created a model to predict text

--- Tensorflow/NLP2.py
+++ Tensorflow/NLP2.py
@@ -73,3 +73,113 @@ BUFFER_SIZE = 10000
 
 data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
+# Building the model
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.LSTM(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+model = build_model(VOCAB_SIZE,EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
+model.summary()
+
+# Loss function
+for input_example_batch, target_example_batch in data.take(1):
+  example_batch_predictions = model(input_example_batch)  # ask our model for a prediction on our first batch of training data (64 entries)
+  #print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")  # print out the output shape
+
+# we can see that the predicition is an array of 64 arrays, one for each entry in the batch
+#print(len(example_batch_predictions))
+#print(example_batch_predictions)
+
+# lets examine one prediction
+#pred = example_batch_predictions[0]
+#print(len(pred))
+#print(pred)
+# notice this is a 2d array of length 100, where each interior array is the prediction for the next character at each time step
+
+# If we want to determine the predicted character we need to sample the output distribution (pick a value based on probabillity)
+sampled_indices = tf.random.categorical(pred, num_samples=1)
+
+# now we can reshape that array and convert all the integers to numbers to see the actual characters
+sampled_indices = np.reshape(sampled_indices, (1, -1))[0]
+predicted_chars = int_to_text(sampled_indices)
+
+predicted_chars  # and this is what the model predicted for training sequence 1
+
+def loss(labels, logits): # Creating the actual loss function
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+# Compiling the model
+model.compile(optimizer='adam', loss=loss)
+
+# Creating checkpoints
+# Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+# Training
+history = model.fit(data, epochs=50, callbacks=[checkpoint_callback])
+
+# Loading the model
+model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, batch_size=1)
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+
+checkpoint_num = 10
+model.load_weights(tf.train.load_checkpoint("./training_checkpoints/ckpt_" + str(checkpoint_num)))
+model.build(tf.TensorShape([1, None]))
+
+# Generating Text
+def generate_text(model, start_string):
+  # Evaluation step (generating text using the learned model)
+
+  # Number of characters to generate
+  num_generate = 800
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+
+  # Low temperatures results in more predictable text.
+  # Higher temperatures results in more surprising text.
+  # Experiment to find the best setting.
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # remove the batch dimension
+    
+      predictions = tf.squeeze(predictions, 0)
+
+      # using a categorical distribution to predict the character returned by the model
+      predictions = predictions / temperature
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+      # We pass the predicted character as the next input to the model
+      # along with the previous hidden state
+      input_eval = tf.expand_dims([predicted_id], 0)
+
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+inp = input("Type a starting string: ")
+print(generate_text(model, inp))
